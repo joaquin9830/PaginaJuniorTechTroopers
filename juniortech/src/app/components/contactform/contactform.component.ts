f1c19de8f131cc623958835b68d036c5c4b5bac5
joaquin9830 <joaquin9811@gmail.com>
2024-07-04 15:08:52 -0300

    Formulario de contacto sin terminar

--- juniortech/src/app/components/contactform/contactform.component.ts
+++ juniortech/src/app/components/contactform/contactform.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import emailjs, { EmailJSResponseStatus } from 'emailjs-com';
 
 @Component({
   selector: 'app-contactform',
@@ -12,10 +13,9 @@ export class ContactformComponent implements OnInit{
   constructor(private fb: FormBuilder) {
     this.contactForm = this.fb.group({
       name: ['', Validators.required],
-      email: ['', [Validators.required, Validators.email]],      
-      contactNumber: ['', [Validators.required, Validators.pattern('[0-9]{10}')]],    
-      why: ['', Validators.required],     
-      
+      email: ['', [Validators.required, Validators.email]],
+      contactNumber: ['', [Validators.required, Validators.pattern('[0-9]{10}')]],
+      why: ['', Validators.required]
     });
   }
 
@@ -23,8 +23,21 @@ export class ContactformComponent implements OnInit{
 
   onSubmit(): void {
     if (this.contactForm.valid) {
-      console.log('Formulario enviado', this.contactForm.value);
-      
+      const templateParams = {
+        name: this.contactForm.get('name')?.value,
+        email: this.contactForm.get('email')?.value,
+        contactNumber: this.contactForm.get('contactNumber')?.value,
+        why: this.contactForm.get('why')?.value
+      };
+
+      emailjs.send('YOUR_SERVICE_ID', 'template_5ucuhgp', templateParams, 'YOUR_USER_ID')
+        .then((response: EmailJSResponseStatus) => {
+          console.log('Email enviado', response.status, response.text);
+          alert('Email enviado con éxito.');
+        }, (error) => {
+          console.error('Error al enviar el email', error);
+          alert('Hubo un error al enviar el email.');
+        });
     } else {
       console.log('Formulario no válido');
     }
