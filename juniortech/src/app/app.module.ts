efd3b3e34da4aaa9ae9fced7bdde8fe769d741aa
joaquin9830 <joaquin9811@gmail.com>
2024-07-03 23:25:05 -0300

    Modificación de header | Nuevo componente carousel

--- juniortech/src/app/app.module.ts
+++ juniortech/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { AboutComponent } from './components/about/about.component';
+import { CarouselComponent } from './components/carousel/carousel.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    AboutComponent
+    AboutComponent,
+    CarouselComponent
   ],
   imports: [
     BrowserModule,
