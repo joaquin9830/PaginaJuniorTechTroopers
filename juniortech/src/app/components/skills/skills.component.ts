6ee6e7faa9f29ec9c38fa42d7027c1fc71be510c
joaquin9830 <joaquin9811@gmail.com>
2024-07-04 13:43:58 -0300

    Sección skills con efectos | iconos actualizados

--- juniortech/src/app/components/skills/skills.component.ts
+++ juniortech/src/app/components/skills/skills.component.ts
@@ -6,5 +6,33 @@ import { Component } from '@angular/core';
   styleUrl: './skills.component.css'
 })
 export class SkillsComponent {
-
+  skills = [
+    {
+      name: 'Frontend',
+      items: [
+        { name: 'HTML', image: 'assets/images/skills/html.png', levelIcons: ['bi bi-star-fill', 'bi bi-star-fill', 'bi bi-star-fill', 'bi bi-star'] },
+        { name: 'CSS', image: 'assets/images/skills/css.png', levelIcons: ['bi bi-star-fill', 'bi bi-star-fill', 'bi bi-star-fill', 'bi bi-star'] },
+        { name: 'JavaScript', image: 'assets/images/skills/javascript.png', levelIcons: ['bi bi-star-fill', 'bi bi-star-fill', 'bi bi-star-fill', 'bi bi-star'] },
+        { name: 'Angular', image: 'assets/images/skills/angular.png', levelIcons: ['bi bi-star-fill', 'bi bi-star-fill', 'bi bi-star-fill', 'bi bi-star'] }
+      ]
+    },
+    {
+      name: 'Backend',
+      items: [
+        { name: 'Node.js', image: 'path/to/nodejs.png', levelIcons: ['bi bi-star-fill', 'bi bi-star-fill', 'bi bi-star'] },
+        { name: 'Python', image: 'path/to/python.png', levelIcons: ['bi bi-star-fill', 'bi bi-star-fill', 'bi bi-star'] },
+        { name: 'Java', image: 'path/to/java.png', levelIcons: ['bi bi-star-fill', 'bi bi-star-fill', 'bi bi-star'] },
+        { name: 'PHP', image: 'path/to/php.png', levelIcons: ['bi bi-star-fill', 'bi bi-star-fill', 'bi bi-star'] }
+      ]
+    },
+    {
+      name: 'DB',
+      items: [
+        { name: 'MySQL', image: 'path/to/mysql.png', levelIcons: ['bi bi-star-fill', 'bi bi-star-fill', 'bi bi-star-fill'] },
+        { name: 'MongoDB', image: 'path/to/mongodb.png', levelIcons: ['bi bi-star-fill', 'bi bi-star-fill'] },
+        { name: 'PostgreSQL', image: 'path/to/postgresql.png', levelIcons: ['bi bi-star-fill', 'bi bi-star-fill', 'bi bi-star'] },
+        { name: 'SQLite', image: 'path/to/sqlite.png', levelIcons: ['bi bi-star-fill', 'bi bi-star-fill', 'bi bi-star'] }
+      ]
+    }
+  ];
 }
