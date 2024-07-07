be42721e25cea1f37a73af630b3d9abe8f9f1fd1
joaquin9830 <joaquin9811@gmail.com>
2024-07-07 16:05:41 -0300

    Nueva scrollbar

--- juniortech/src/app/components/home/home.component.ts
+++ juniortech/src/app/components/home/home.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        window.scrollTo(0, 0); // Forzar desplazamiento superior
+      }
+    });
+  }
 }
